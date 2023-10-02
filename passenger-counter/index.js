82cfbe40eb06cc578ae8b35656315448324ace05
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-02 06:28:53 +0200

    Substitute innerText with textContext after learning about its better performance

--- passenger-counter/index.js
+++ passenger-counter/index.js
@@ -5,11 +5,11 @@ let countEl = document.getElementById('count-el');
 function decrement() {
   if (count > 0) {
     count -= 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
   }
 }
 
 function increment() {
   count += 1;
-  countEl.innerText = count;
+  countEl.textContent = count;
 }
