5bb1d575d30ff768c7f7b518272b846dd7a51553
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-03 07:44:53 +0200

    Add project base - WIP

---
+++ backjack-game/index.js
@@ -0,0 +1,21 @@
+let firstCard = 7;
+let secondCard = 11;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = '';
+let messageEl = document.getElementById('message-el');
+
+function startGame() {
+  if (sum <= 20) {
+    message = 'Do you want to draw a new card? 🙂';
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
