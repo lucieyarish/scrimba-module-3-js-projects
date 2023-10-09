9bef68a6ceffbdc587273f41179d15689b0ba6fe
Lucie Yarish <lucieyarish@hotmail.com>
2023-10-09 17:13:17 +0200

    Finish game logic, adjust & add styling

--- backjack-game/index.js
+++ backjack-game/index.js
@@ -1,12 +1,48 @@
-let firstCard = 7;
-let secondCard = 11;
-let sum = firstCard + secondCard;
+let player = {
+  name: 'Las Vegas Joe',
+  chips: 357,
+};
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = '';
 let messageEl = document.getElementById('message-el');
+let cardsEl = document.getElementById('cards-el');
+let sumEl = document.getElementById('sum-el');
+let playerEl = document.getElementById('player-el');
+
+playerEl.textContent = player.name + ': $' + player.chips;
+
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+
+  if (randomCard > 10) {
+    return 10;
+  } else if (randomCard === 1) {
+    return 11;
+  } else {
+    return randomCard;
+  }
+}
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = 'Cards: ';
+
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + ' ';
+  }
+
+  sumEl.textContent = 'Sum: ' + sum;
   if (sum <= 20) {
     message = 'Do you want to draw a new card? 🙂';
   } else if (sum === 21) {
@@ -18,4 +54,13 @@ function startGame() {
   }
 
   messageEl.textContent = message;
+}
+
+function newCard() {
+  if (isAlive && !hasBlackJack) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
